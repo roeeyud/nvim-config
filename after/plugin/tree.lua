1877da6f99e3a0b5dc653b88758a3e6aee1b01ac
roeeyudilevitch <roeeyud@gmail.com>
2023-06-07 15:54:20 +0300

    better config

--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -1,7 +1,10 @@
 require("nvim-tree").setup({
    git = {
        ignore = false
-   }
+   },
+   view = {
+    width = 70,
+  },
 })
 vim.keymap.set("n", "<leader>w", "<cmd>NvimTreeToggle<cr>",
   {silent = true, noremap = true})
